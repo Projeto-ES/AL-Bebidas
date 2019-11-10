bd0f0f205fec2aa345324b5852f2417f563fda97
EvertonTenorio <everton.tenorio10@gmail.com>
2019-11-10 15:19:52 -0300

    concertado cenario de vizualizar produtos

--- features/step_definitions/produto_steps.rb
+++ features/step_definitions/produto_steps.rb
@@ -1,15 +1,20 @@
 Given("Eu estou na pagina de produtos")do
   visit '/produtos'
+  expect(page).to have_content('Produtos')
 end
-And ("Eu visualizo o produto de nome coca-cola") do
+And ("O produto de codigo {string}, nome {string} e valor {string} existe") do |codigo, nome, valor|
+  click_link 'Novo Produto'
+  fill_in 'produto[codigo]', :with => codigo
+  fill_in 'produto[nome]', :with => nome
+  fill_in 'produto[valor]', :with => valor
+  click_button 'Create Produto'
   visit '/produtos'
-  expect(page).to have_content('coca-cola')
+  expect(page).to have_content(nome)
 end
 
-When ("Eu clico em visualizar o produto de nome coca-cola") do
-  click_link 'Visualizar'
+When ("Eu clico em visualizar o produto de nome {string}") do |nome|
+  click_link "v-#{nome}"
 end
-Then ("Eu vejo todas as informações do produto") do
-  visit 'produtos/show'
-  expected(page).to have_contente('coca-cola')
+Then ("Eu vejo todas as informações do produto de nome {string}") do |nome|
+  expect(page).to have_content(nome)
 end
